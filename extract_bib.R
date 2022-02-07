6dc97ca2677c75d9c33f36b83cfedc89f04be3f2
chainsawriot <chainsawtiney@gmail.com>
2022-02-07 15:31:58 +0100

    Move extensions and LOO to `extension.rmd`

--- extract_bib.R
+++ extract_bib.R
@@ -1,1 +1,1 @@
-condensebib::reduce_bib("manuscript.rmd", master_bib = "/home/chainsawriot/dev/dotfiles/bib.bib", out_bib = "manuscript.bib")
+condensebib::reduce_bib(c("manuscript.rmd", "extension.rmd"), master_bib = "/home/chainsawriot/dev/dotfiles/bib.bib", out_bib = "manuscript.bib")
--- wjs2.R
+++ wjs2.R
@@ -4,3 +4,11 @@ x <- readRDS("ppa_qmod.RDS")
 qmod_condit <- plot(conditional_effects(x, effects = "eiu", ask = FALSE, plot = FALSE))
 
 saveRDS(qmod_condit, "qmod_condit.RDS")
+
+## LOO
+
+ppa_mod <- readRDS("ppa_mod.RDS")
+ppa_qmod <- readRDS("ppa_qmod.RDS")
+
+saveRDS(loo(ppa_qmod, ppa_mod), "ppa_loo.RDS")
+
